3f041255bd8a4cd99eff39b2323da9d10d476237
Kylie Oeffie <kylieuffie@gmail.com>
2026-02-05 21:37:55 -0800

    Storyboard made, joke state properties added

--- iKid/ViewController.swift
+++ iKid/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    @IBOutlet weak var jokeLabel: UILabel!
+    
+    var jokes: [(question: String, answer: String)] = []
+    var currentIndex = 0
+    var showingAnswer = false
+    
 }
 
